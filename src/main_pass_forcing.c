aac1beac23f10486187080eb25ed2052aea0e89f
Jonathan Frame <jmframe@crimson.ua.edu>
2021-09-10 12:35:49 -0600

    adding aorc bmi code to pass forcing to pet

--- src/main_pass_forcing.c
+++ src/main_pass_forcing.c
@@ -5,8 +5,8 @@
 #include "../include/pet.h"
 #include "../include/bmi_pet.h"
 
-#include "../../AORC/include/aorc.h"
-#include "../../AORC/include/bmi_aorc.h"
+#include "../forcing_code/include/aorc.h"
+#include "../forcing_code/include/bmi_aorc.h"
 
 /***************************************************************
     Function to pass the forcing data from AORC to PET using BMI.
