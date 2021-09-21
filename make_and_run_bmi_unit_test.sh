8062b49ab6534c41c2d0c22ca8db87c9808ed2e6
madMatchstick <jlgred88@gmail.com>
2021-09-21 17:28:31 -0600

    bmi unit test

--- make_and_run_bmi_unit_test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-gcc -lm ./src/main_unit_test_bmi.c ./src/bmi_pet.c ./src/pet.c -o run_pet_bmi_test
-#./run_pet_bmi_test ./configs/pet_config_unit_test1.txt
-#./run_pet_bmi_test ./configs/pet_config_unit_test2.txt
-#./run_pet_bmi_test ./configs/pet_config_unit_test3.txt
-#./run_pet_bmi_test ./configs/pet_config_unit_test4.txt
-#./run_pet_bmi_test ./configs/pet_config_unit_test5.txt
-./run_pet_bmi_test ./configs/pet_config_cat_67.txt
