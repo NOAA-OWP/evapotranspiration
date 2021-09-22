b11064b4c371e0cbb19cc69003b83ca7640d7350
madMatchstick <jlgred88@gmail.com>
2021-09-22 09:07:20 -0600

    rm configs/ forcing/ from test/ dir

--- test/make_and_run_bmi_unit_test.sh
+++ test/make_and_run_bmi_unit_test.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 gcc -lm ./main_unit_test_bmi.c ../src/bmi_pet.c ../src/pet.c -o run_pet_bmi_test
-./run_pet_bmi_test ./configs/pet_config_bmi_method1.txt
-./run_pet_bmi_test ./configs/pet_config_bmi_method2.txt
-./run_pet_bmi_test ./configs/pet_config_bmi_method3.txt
-./run_pet_bmi_test ./configs/pet_config_bmi_method4.txt
-./run_pet_bmi_test ./configs/pet_config_bmi_method5.txt
-./run_pet_bmi_test ./configs/pet_config_cat_67_method1.txt
-./run_pet_bmi_test ./configs/pet_config_cat_67_method2.txt
-./run_pet_bmi_test ./configs/pet_config_cat_67_method3.txt
-./run_pet_bmi_test ./configs/pet_config_cat_67_method4.txt
-./run_pet_bmi_test ./configs/pet_config_cat_67_method5.txt
+./run_pet_bmi_test ../configs/pet_config_bmi_unit_test.txt
+#./run_pet_bmi_test ../configs/pet_config_cat_67.txt
