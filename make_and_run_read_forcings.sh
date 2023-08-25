1df115fdd5376fe37be8ceafcb5df3b2dd0577e4
JessicaGarrett-NOAA <30940444+madMatchstick@users.noreply.github.com>
2023-08-25 14:05:21 -0600

    GH Workflow (#25)
    * initial workflow
    
    * Update build_and_run_tests.yml
    
    * Update build_and_run_tests.yml
    
    * Update build_and_run_tests.yml
    
    * Update build_and_run_tests.yml
    
    * Update build_and_run_tests.yml
    
    * change order of compile keystrokes; need to know
    
    * math lib (`-lm`) flag should be listed after compile files
    
    * Update build_and_run_tests.yml
    
    * Update build_and_run_tests.yml
    
    * Update build_and_run_tests.yml
    
    * Update build_and_run_tests.yml
    
    * Update build_and_run_tests.yml
    
    * Update build_and_run_tests.yml
    
    * Update build_and_run_tests.yml
    
    * Update build_and_run_tests.yml
    
    * Update build_and_run_tests.yml
    
    * Update build_and_run_tests.yml
    
    * Update make_and_run_read_forcings.sh
    
    * Update make_and_run_read_forcings.sh
    
    * Update make_and_run_read_forcings.sh
    
    * Update make_and_run_pass_forcings.sh
    
    * Update build_and_run_tests.yml

--- make_and_run_read_forcings.sh
+++ make_and_run_read_forcings.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-gcc -lm ./src/main_read_forcing.c ./src/pet.c ./src/bmi_pet.c -o run_bmi_forcings_read
+gcc ./src/main_read_forcing.c ./src/pet.c ./src/bmi_pet.c -lm -o run_bmi_forcings_read
 ./run_bmi_forcings_read ./configs/pet_config_unit_test1.txt 
 ./run_bmi_forcings_read ./configs/pet_config_unit_test2.txt 
 ./run_bmi_forcings_read ./configs/pet_config_unit_test3.txt 
--- test/make_and_run_bmi_unit_test.sh
+++ test/make_and_run_bmi_unit_test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-gcc -lm ./main_unit_test_bmi.c ../src/bmi_pet.c ../src/pet.c -o run_pet_bmi_test
+gcc ./main_unit_test_bmi.c ../src/bmi_pet.c ../src/pet.c -lm -o run_pet_bmi_test
 ./run_pet_bmi_test ../configs/pet_config_bmi_unit_test.txt
 #./run_pet_bmi_test ../configs/pet_config_cat_67.txt
