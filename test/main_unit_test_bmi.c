91a4c10d6a12d96c49bf4166510c822682229618
Phil Miller <philip.miller@noaa.gov>
2024-03-29 11:57:36 -0700

    Print pointers as pointers, not confused doubles

--- test/main_unit_test_bmi.c
+++ test/main_unit_test_bmi.c
@@ -282,7 +282,7 @@ main(int argc, const char *argv[]){
             {
                 status = model->get_value_ptr(model, var_name, (void**)(&var));
                 if (status == BMI_FAILURE)return BMI_FAILURE;
-                printf("   get value ptr: %f\n",var);
+                printf("   get value ptr: %p\n",var);
             }
             // Go ahead and test set_value_*() for last time step here
             if (n == test_nstep){
@@ -346,7 +346,7 @@ main(int argc, const char *argv[]){
             {
                 status = model->get_value_ptr(model, var_name, (void**)(&var));
                 if (status == BMI_FAILURE)return BMI_FAILURE;
-                printf("   get value ptr: %f\n",var);
+                printf("   get value ptr: %p\n",var);
             }
             // Go ahead and test set_value_*() for last time step here
             if (n == test_nstep){
