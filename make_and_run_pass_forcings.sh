140e90a14f9983304853dd81f10653f39af84f08
Phil Miller <philip.miller@noaa.gov>
2024-03-29 11:44:40 -0700

    Compile with -Werror to prevent introducing new warnings

--- make_and_run_pass_forcings.sh
+++ make_and_run_pass_forcings.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gcc ./src/main_pass_forcing.c ./src/pet.c ./src/bmi_pet.c ./extern/forcing_code/src/aorc.c ./extern/forcing_code/src/bmi_aorc.c -lm -o run_bmi_forcings_pass
+gcc -Werror ./src/main_pass_forcing.c ./src/pet.c ./src/bmi_pet.c ./extern/forcing_code/src/aorc.c ./extern/forcing_code/src/bmi_aorc.c -lm -o run_bmi_forcings_pass
 ./run_bmi_forcings_pass ./configs/pet_config_bmi.txt ./configs/aorc_config_cat_67.txt 
